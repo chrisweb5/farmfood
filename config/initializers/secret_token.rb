6de86e2ddf16dc54b7feb547afb74e91073083b7
Christian Weber <C_W_84@hotmail.com>
2013-10-05 17:50:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Farmfood::Application.config.secret_key_base = 'a7ea42f0dc53d13f69dfc76f32cb1c466068ada7e06125e73fcc3cbe2e4277920e45319d58f7fa882fdb0d8d305a257167e9cd528eec08dd81e798188b517eb7'
