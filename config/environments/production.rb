c80703d86116b115832fb7728f9475ae0931f4d0
Christian Weber <C_W_84@hotmail.com>
2013-10-07 18:23:04 +0200

    try to show error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,8 +11,8 @@ Farmfood::Application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local       = true #false
+  config.action_controller.perform_caching = false #true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
