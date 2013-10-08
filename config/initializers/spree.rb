4bf8f7ce982901ab721d65a94f5bb1b22328537f
Christian Weber <C_W_84@hotmail.com>
2013-10-08 14:49:15 +0200

    try fix amazon

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -5,14 +5,14 @@
 #
 # In order to initialize a setting do:
 # config.setting_name = 'new value'
-#Spree.config do |config|
-#  config.use_s3 = true
-#  config.s3_bucket = ENV['AWS_BUCKET'],
- # config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
- # config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
- # config.attachment_url = ":s3_eu_url"
- # config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
-#end
+Spree.config do |config|
+  config.use_s3 = true
+  config.s3_bucket = ENV['AWS_BUCKET']
+  config.s3_access_key = ENV['AWS_ACCESS_KEY_ID']
+  config.s3_secret = ENV['AWS_SECRET_ACCESS_KEY']
+  config.attachment_url = ":s3_eu_url"
+  config.s3_host_alias = "s3-eu-west-1.amazonaws.com"
+end
 
 Paperclip.interpolates(:s3_eu_url) do |attachment, style|
  "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
