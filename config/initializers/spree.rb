2fbd863165e785f743b89682d405b74927f2651a
Christian Weber <C_W_84@hotmail.com>
2013-10-08 14:51:42 +0200

    try fix amazon

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -16,6 +16,6 @@ end
 
 Paperclip.interpolates(:s3_eu_url) do |attachment, style|
  "#{attachment.s3_protocol}://#{Spree::Config[:s3_host_alias]}/#{attachment.bucket_name}/#{attachment.path(style).gsub(%r{^/},"")}"
-Send
+end
 
 Spree.user_class = "Spree::User"
