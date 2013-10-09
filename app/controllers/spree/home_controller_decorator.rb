a1af78b35ec749ec92e29199b320b0bd4429b796
Christian Weber <C_W_84@hotmail.com>
2013-10-09 22:01:38 +0200

    add searcher

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -4,11 +4,18 @@ Spree::HomeController.class_eval do
     slider = Spree::Taxon.where(:name => 'Slider').first
     @slider_products = slider.products.active if slider
 
-   # featured = Spree::Taxon.where(:name => 'Featured').first
-   # @featured_products = featured.products.active if featured
+   featured = Spree::Taxon.where(:name => 'Featured').first
+   @featured_products = featured.products.active if featured
 
-   # latest = Spree::Taxon.where(:name => 'Latest').first
-   # @latest_products = latest.products.active if latest
+   latest = Spree::Taxon.where(:name => 'Latest').first
+   @latest_products = latest.products.active if latest
+
+
+   @searcher = Spree::Config.searcher_class.new(params)
+	@searcher.current_user = try_spree_current_user
+	@searcher.current_currency = current_currency
+	@products = @searcher.retrieve_products
+	respond_with(@products)
   end
 
 end
