ff4bc7d67625ef02e5bdf71cf2127ea84fc4b310
Christian Weber <C_W_84@hotmail.com>
2013-10-09 10:51:55 +0200

    change production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,6 @@ Farmfood::Application.configure do
   }
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.debug = true
 end
