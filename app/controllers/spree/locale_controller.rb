197a4599daaec9d4ecaee8b01c134c7ad1c00b0f
Christian Weber <C_W_84@hotmail.com>
2013-10-09 23:18:33 +0200

    add locale_controller

--- app/controllers/spree/locale_controller.rb
+++ app/controllers/spree/locale_controller.rb
@@ -1,16 +1,12 @@
 module Spree
   class LocaleController < Spree::StoreController
     def set
-      #if request.referer && request.referer.starts_with?('http://' + request.host)
-       # session['user_return_to'] = request.referer
-      #end
-      if params[:locale] && I18n.available_locales.map(&:to_s).include?(params[:locale])
-        session[:locale] = I18n.locale = params[:locale]
-        flash.notice = Spree.t(:locale_changed)
-      else
-        flash[:error] = Spree.t(:locale_not_changed)
+      session[:locale] = params[:locale]
+
+      respond_to do |format|
+        format.json { render :json => true }
+        format.html { redirect_to root_path }
       end
-      redirect_back_or_default(spree.root_path)
     end
   end
 end
