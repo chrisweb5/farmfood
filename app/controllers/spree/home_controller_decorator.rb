ea9e6cd81632b2ffaa5d9549bbeb53a71ec81df6
Christian Weber <C_W_84@hotmail.com>
2013-10-09 22:41:02 +0200

    fix home controller decorator

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,21 +1,22 @@
 Spree::HomeController.class_eval do
+require 'spree/base_controller'
 
-  def index
-    slider = Spree::Taxon.where(:name => 'Slider').first
-    @slider_products = slider.products.active if slider
+def index
+slider = Spree::Taxon.where(:name => 'Slider').first
+@slider_products = slider.products.active if slider
 
-   featured = Spree::Taxon.where(:name => 'Featured').first
-   @featured_products = featured.products.active if featured
+featured = Spree::Taxon.where(:name => 'Featured').first
+@featured_products = featured.products.active if featured
 
-   latest = Spree::Taxon.where(:name => 'Latest').first
-   @latest_products = latest.products.active if latest
+latest = Spree::Taxon.where(:name => 'Latest').first
+@latest_products = latest.products.active if latest
 
 
-   @searcher = Spree::Config.searcher_class.new(params)
-	@searcher.current_user = try_spree_current_user
-	@searcher.current_currency = current_currency
-	@products = @searcher.retrieve_products
-	respond_with(@products)
-  end
+@searcher = Spree::Config.searcher_class.new(params)
+@searcher.current_user = try_spree_current_user
+@searcher.current_currency = current_currency
+@products = @searcher.retrieve_products
+respond_with(@products)
+end
 
 end
