b1af51164f8feb41771a7e11c8df80694a5d5eed
Christian Weber <C_W_84@hotmail.com>
2013-10-15 22:36:49 +0200

    chanfe application rb

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'app/models/spree/review_sweeper'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+
 
 module Farmfood
   class Application < Rails::Application
