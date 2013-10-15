39133646eed3de8f9db2ace7ed3f3b410bf02917
Christian Weber <C_W_84@hotmail.com>
2013-10-15 20:53:42 +0200

    config requie sweeper

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,5 +1,5 @@
 #require for spree_reviews
-require File.dirname(__FILE__) + '/review_sweeper.rb'
+#require File.dirname(__FILE__) + '/review_sweeper.rb'
 # Configure Spree Preferences
 #
 # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
