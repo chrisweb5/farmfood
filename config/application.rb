3ddac6ced6774fd22ffa75bc4cb52b81afdf4e91
Christian Weber <C_W_84@hotmail.com>
2013-10-15 17:51:37 +0200

    config sweeper

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module Farmfood
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
+    #fix actioncontroller missing initialize sweeper
+    config.autoload_paths += %W( #{Rails.root}/path/to/sweepers )
     #changed to config/initializers/locale.rb
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     #config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
